d45806740a276bab2f2d156560ca9dbd7d4a639b
muhammadderic <muhammad.donny.e@gmail.com>
2025-05-20 11:11:11 +0700

    f: create Header

--- client/app/(root)/documents/[id]/page.tsx
+++ client/app/(root)/documents/[id]/page.tsx
@@ -1,8 +1,16 @@
 import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/Header";
 
 const Documents = () => {
   return (
     <div>
+      <Header className="sticky left-0 top-0">
+        <div className="flex items-center gap-2 lg:gap-4">
+          {/* TODO: Add notification comp */}
+          {/* TODO: Add sign-in comp */}
+        </div>
+      </Header>
+
       <Editor />
     </div>
   )
